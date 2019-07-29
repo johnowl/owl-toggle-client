7e5d560199718b70ffcf72c643189301e4d3731b
João Paulo <johnowl@gmail.com>
2019-07-29 18:50:37 -0300

    Remove unused import.

--- src/main/kotlin/com/johnowl/toggle/client/FeatureToggleIntegration.kt
+++ src/main/kotlin/com/johnowl/toggle/client/FeatureToggleIntegration.kt
@@ -3,7 +3,6 @@ package com.johnowl.toggle.client
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
-import org.springframework.web.client.RestTemplate
 
 @Service
 class FeatureToggleIntegration {
