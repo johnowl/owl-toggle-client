5c46a729bfc0c731c4b13a1e9165cd84f3b42ac1
João Paulo <johnowl@gmail.com>
2019-07-29 18:51:31 -0300

    Refator to use primary constructor.

--- src/main/kotlin/com/johnowl/toggle/client/FeatureToggleClient.kt
+++ src/main/kotlin/com/johnowl/toggle/client/FeatureToggleClient.kt
@@ -1,17 +1,9 @@
 package com.johnowl.toggle.client
 
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 @Service
-class FeatureToggleClient {
-
-    private val integration: FeatureToggleIntegration
-
-    @Autowired
-    constructor(integration: FeatureToggleIntegration) {
-        this.integration = integration
-    }
+class FeatureToggleClient(private val integration: FeatureToggleIntegration) {
 
     fun sendVariables(userId: String, variables: Map<String, Any>) {
         return integration.sendVariables(userId, variables)
